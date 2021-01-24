374a6ab392a94b907626428d00ba297538119137
Mayara Araujo <mayaramaaraujo98@gmail.com>
2021-01-24 20:37:35 -0300

    carousel feito

--- frontend/src/components/Principal/PrincipaisNoticias/Destaques/Destaques.jsx
+++ frontend/src/components/Principal/PrincipaisNoticias/Destaques/Destaques.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { CaixaDestaques } from '../../styled-principal';
 import DestaquePrincipal from './DestaquePrincipal.jsx/DestaquePrincipal';
 import DestaqueSecundario from './DestaqueSecundario/DestaqueSecundario';
@@ -6,6 +6,18 @@ import DestaqueSecundario from './DestaqueSecundario/DestaqueSecundario';
 const mockDestaques = [
     {   
         imagem: "https://picsum.photos/500/430",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum orci euismod eros commodo tincidunt."
+    },
+    {   
+        imagem: "https://picsum.photos/500/431",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum orci euismod eros commodo tincidunt."
+    },
+    {   
+        imagem: "https://picsum.photos/500/433",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum orci euismod eros commodo tincidunt."
+    },
+    {   
+        imagem: "https://picsum.photos/500/434",
         descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum orci euismod eros commodo tincidunt."
     }
 ]
@@ -17,23 +29,40 @@ const mockSecundario = {
 }
 
 function Destaques() {
+    const [tamanho, setTamanho] = useState(mockDestaques.length - 1) //4
+    const [imagemIndex, setImagemIndex] = useState(0) //0
+
+    //console.log(tamanho)
+
+    const ProximaImagem = () => {
+        if(imagemIndex === tamanho){
+            setImagemIndex(0)
+        } else {
+            setImagemIndex(imagemIndex + 1)
+        }      
+    }
+
+    useEffect(() => {
+        const intervaloId = setInterval(() => {
+            ProximaImagem()
+        }, 5000)
+
+        return () => clearInterval(intervaloId)
+    }, [imagemIndex])
+
 
     return (
         <CaixaDestaques>
-           {mockDestaques.map((item) => {
-               return (
-                   <DestaquePrincipal
-                        imagem={item.imagem}
-                        descricao={item.descricao}
-                    />
-               )
-           })}
+            <DestaquePrincipal
+                imagem={mockDestaques[imagemIndex].imagem}
+                descricao={mockDestaques[imagemIndex].descricao}
+            />
 
-        <DestaqueSecundario 
-            titulo={mockSecundario.titulo}
-            imagem={mockSecundario.imagem}
-            descricao={mockSecundario.descricao}
-        />
+            <DestaqueSecundario 
+                titulo={mockSecundario.titulo}
+                imagem={mockSecundario.imagem}
+                descricao={mockSecundario.descricao}
+            />
         </CaixaDestaques>
     )
 }
